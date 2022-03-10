3da0095fad4b3ae795ab94154eb44a91fd81c815
智盛 許 <bird98n159753@yahoo.com.tw>
2022-03-10 20:58:22 +0800

    新增會員控制器

---
+++ Controllers/memberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GNT_server.Controllers
+{
+    public class memberController : Controller
+    {
+        // GET: asd
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
