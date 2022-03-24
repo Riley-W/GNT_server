b1f38d4324ef8d2f1be38006318fbab75c8ec80c
Yihsuan <b10317033@gapps.ntust.edu.tw>
2022-03-24 14:31:58 +0800

    edit SendOKEmail/SendEditEMail

--- Controllers/WebsiteReviewController.cs
+++ Controllers/WebsiteReviewController.cs
@@ -87,8 +87,11 @@ namespace GNT_server.Controllers
                 WebsiteReview.Status = "未處理";
                 db.WebsiteReview.Add(WebsiteReview);
                 await db.SaveChangesAsync();
-
-               // SendEmail();
+                var mailAddress = db.MemberInfo.Find(WebsiteReview.MemberID).Email.ToString();
+                var receiver = db.MemberInfo.Find(WebsiteReview.MemberID).Name.ToString();
+                DateTime receiveDateTime = (DateTime)WebsiteReview.ReviewDate;
+                var receiveDate = receiveDateTime.ToString("yyyy/MM/dd");
+                SendOKEmail(mailAddress, receiver, WebsiteReview.RContent, receiveDate);
 
                 //return CreatedAtRoute("DefaultApi", new { MemberID = WebsiteReview.MemberID, ReviewDate = WebsiteReview.ReviewDate, Type = WebsiteReview.Type, RContent = WebsiteReview.RContent, Status = WebsiteReview.Status}, WebsiteReview);
                 return Ok("資料新增完成。");
@@ -145,9 +148,24 @@ namespace GNT_server.Controllers
             try 
             { 
                 await db.SaveChangesAsync();
-                var mailAddress = db.MemberInfo.Find(WebsiteReview.MemberID).Email.ToString();
-                var receiver = db.MemberInfo.Find(WebsiteReview.MemberID).Name.ToString();
-                SendEmail(mailAddress, receiver, WebsiteReview.RContent);
+
+                if (WebsiteReview.Status == "已處理")
+                {
+                    var mailAddress = db.MemberInfo.Find(WebsiteReview.MemberID).Email.ToString();
+                    var receiver = db.MemberInfo.Find(WebsiteReview.MemberID).Name.ToString();
+                    
+                    if (WebsiteReview.Remark == null)
+                    {
+                        return BadRequest("系統回覆不能為空。");
+                    }
+                    var Reply = WebsiteReview.Remark.ToString();
+                    DateTime receiveDateTime = (DateTime)WebsiteReview.ReviewDate;
+                    var receiveDate = receiveDateTime.ToString("yyyy/MM/dd");
+                    SendEditEmail(mailAddress, receiver, WebsiteReview.RContent, Reply, receiveDate);
+                }
+                
+
+
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -166,13 +184,14 @@ namespace GNT_server.Controllers
 
         }
 
+        
 
         private bool WebsiteReviewExits(int ReviewID)
         {
             return db.WebsiteReview.Count(e => e.ReviewID == ReviewID) > 0;
         }
 
-        private void SendEmail(string mailAddress, string receiver, string reviewContent)
+        private void SendOKEmail(string mailAddress, string receiver, string reviewContent, string receiveDate)
         {
             try
             {
@@ -200,17 +219,68 @@ namespace GNT_server.Controllers
                 myMail.ReplyToList.Add(replyTo);
 
                 //set subject and encoding
-                myMail.Subject = "【系統通知】好夜台南 Good Night Tainan 已收到您的意見回饋。";
+                myMail.Subject = "【好夜台南 Good Night Tainan】 已收到意見回饋";
                 myMail.SubjectEncoding = System.Text.Encoding.UTF8;
 
                 //set body message and encoding
 
-                myMail.Body = "<h3>HI, " + receiver + ",</h3><p>    已收到您的意見回饋，感謝您使用【好夜台南 Good Night Tainan】，您寶貴的意見是我們進步的動力。</p><div align =\"center\" border=\"2px solid black\"> 意見回饋內容: " + reviewContent + "</div><div> -------------此為系統自動發送，請勿直接回覆。為了確保能收到來自【好夜台南 Good Night Tainan】的信件，請將goodnighttainan@gmail.com加入您的通訊錄-------------</div>";
+                myMail.Body = "<div style=\" padding: 24px; margin: auto 50px; border: 2px transparent solid; text-align: center  \">" + "<h2>HI, " + receiver + ",</h2><p>    意見回饋已收到，感謝您使用【好夜台南 Good Night Tainan】，您寶貴的意見是我們進步的動力。 </p><div style=\"border: 4px #E08E45 solid; background-color:#F8F4A6; border-radius: 12px; margin: 24px 200px ; padding: 24px; text-align: left; font-size: 16px; color : black\"> △回饋時間:" + receiveDate + "<br>△回饋內容: " + reviewContent + "<br><br><br><br>瀏覽<a href=\"https://riley-w.github.io/goodNightTainan_front/\">【好夜台南 Good Night Tainan】</a></div></div><div style=\"line-height:15px ; color: grey; margin-top: 20px\"> -----------------------------此為系統自動發送，請勿直接回覆。為了確保能收到來自【好夜台南 Good Night Tainan】的信件，請將goodnighttainan@gmail.com加入您的通訊錄-----------------------------</div>";
+
                 myMail.BodyEncoding = System.Text.Encoding.UTF8;
 
                 //text or html;
                 myMail.IsBodyHtml = true;
-                
+                mySmtpClient.Send(myMail);
+
+            }
+            catch (SmtpException ex)
+            {
+                throw new ApplicationException("smtpException has occured: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void SendEditEmail(string mailAddress, string receiver, string reviewContent, string Reply, string receiveDate)
+        {
+            try
+            {
+                SmtpClient mySmtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+                //set smtp-client with basicAuthentication
+
+                //NetworkCredential LoginInfo = new NetworkCredential("goodnighttainan@gmail.com", "P@ssw0rd-iii87");
+                //NetworkCredential LoginInfo = new NetworkCredential(Convert.ToString(ConfigurationManager.AppSettings["goodnighttainan@gmail.com"]), Convert.ToString(ConfigurationManager.AppSettings["filgqxrexlhpuxso"]));
+                NetworkCredential LoginInfo = new NetworkCredential("goodnighttainan@gmail.com", "filgqxrexlhpuxso");
+
+                mySmtpClient.UseDefaultCredentials = true;
+
+                mySmtpClient.EnableSsl = true; //gmail預設開啟驗證
+                mySmtpClient.Credentials = LoginInfo;
+
+                //add from, to mail addresses
+                MailAddress from = new MailAddress("goodnighttainan@gmail.com", "好夜台南 Good Night Tainan", System.Text.Encoding.UTF8);
+                MailAddress to = new MailAddress(mailAddress, receiver, System.Text.Encoding.UTF8);
+
+                MailMessage myMail = new MailMessage(from, to);
+
+                //add ReplyTo
+                MailAddress replyTo = new MailAddress("goodnighttainan@gmail.com");
+                myMail.ReplyToList.Add(replyTo);
+
+                //set subject and encoding
+                myMail.Subject = "【好夜台南 Good Night Tainan】 已回覆意見回饋";
+                myMail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                //set body message and encoding
+
+                myMail.Body = "<div style=\" padding: 24px; margin: auto 50px; border: 2px transparent solid; text-align: center  \">" + "<h2>HI, " + receiver + ",</h2><p>    感謝您來信【好夜台南 Good Night Tainan】，您的意見回饋已處理： </p><div style=\"border: 4px #E08E45 solid; background-color:#F8F4A6; border-radius: 12px; margin: 24px 200px ; padding: 24px; text-align: left; font-size: 16px; color : black\"> △回饋時間:"+ receiveDate +"<br>△回饋內容: " + reviewContent + "<br>---------------------------------------------------------------------------------------------------<br>△系統回覆: " + Reply + "<br><br><br><br>瀏覽<a href=\"https://riley-w.github.io/goodNightTainan_front/\">【好夜台南 Good Night Tainan】</a></div></div><div style=\"line-height:15px ; color: grey; margin-top: 20px\"> -----------------------------此為系統自動發送，請勿直接回覆。為了確保能收到來自【好夜台南 Good Night Tainan】的信件，請將goodnighttainan@gmail.com加入您的通訊錄-----------------------------</div>";
+                myMail.BodyEncoding = System.Text.Encoding.UTF8;    
+
+                //text or html;
+                myMail.IsBodyHtml = true;
                 mySmtpClient.Send(myMail);
 
             }
