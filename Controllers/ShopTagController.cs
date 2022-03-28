b9b5ea60f1ec3eaf7f4ad1c850ec46f7ae3f1ef4
7305-02\iii <shana5209999@hotmail.com>
2022-03-28 14:32:09 +0800

    new shoptag controller

---
+++ Controllers/ShopTagController.cs
@@ -0,0 +1,47 @@
+﻿using GNT_server.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+
+namespace GNT_server.Controllers
+{
+    [RoutePrefix("api/shoptag")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ShopTagController : ApiController
+    {
+        private projectDBEntities db = new projectDBEntities();
+        /// <summary>
+        /// 查詢店家Tag(渲染)
+        /// </summary>
+        /// <param name="shopid"></param>
+        /// <returns></returns>
+        [Route("Admin/{shopid:int}")]
+        [ResponseType(typeof(ShopInfo))]
+        public IHttpActionResult GetShopInfo(int shopid)
+        {
+            var result = from s in db.ShopTag
+                         join n in db.Tag on s.TagID equals n.TagID
+                         where s.ShopID == shopid
+                         select new 
+                         {
+                             Tag=s.TagID,
+                             TagName=n.TagName
+                         };
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
