32cc0d51e1dddd97e3625f2595fb0a21e23171a2
Yihsuan <b10317033@gapps.ntust.edu.tw>
2022-03-31 15:55:07 +0800

    change date format

---
+++ Models/DateTransfer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GNT_server.Models
+{
+    public class DateTransfer
+    {
+        public static List<object> TransfertoDate(IQueryable<WebsiteReview> origin)
+        {
+            string newDate;
+            List<object> result = new List<object>();
+
+            foreach (var o in origin)
+            {
+                if (o.ReviewDate != null)
+                {
+                    newDate = o.ReviewDate.ToString();
+                    string[] newdatelist = newDate.Split(' ');
+                    result.Add(new
+                    {
+                        ReviewID = o.ReviewID,
+                        MemberID = o.MemberID,
+                        ReviewDate = newdatelist[0],
+                        Type = o.Type,
+                        RContent = o.RContent,
+                        Status = o.Status,
+                        Remark = o.Remark
+                    });
+                }
+                else
+                {
+                    result.Add(o);
+                }
+            }
+
+            return result;
+
+        }
+
+    }
+}
